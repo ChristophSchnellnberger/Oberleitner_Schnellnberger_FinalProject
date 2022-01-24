930ccd9df520ea692062b7d0396318b823bc8b65
Christoph Schnellnberger <s2110438023@fhooe.at>
2022-01-24 11:28:33 +0100

    class Bank construct

--- Oberleitner_Schnellnberger_FinalProject/Oberleitner_Schnellnberger_FinalProject/Bank.cs
+++ Oberleitner_Schnellnberger_FinalProject/Oberleitner_Schnellnberger_FinalProject/Bank.cs
@@ -8,5 +8,60 @@ namespace Oberleitner_Schnellnberger_FinalProject
 {
     internal class Bank
     {
+        #region private members
+        //Kontostand=balance!
+        private double _balancePlayer;
+        private double _balanceCasino;
+        #endregion
+
+        #region public properties
+        public double BalancePlayer
+        {
+            get
+            {
+                return _balancePlayer;
+            }
+            set
+            {
+                //Check correct input!!
+                //kein negativer wert, max. werte, ect...
+            }
+        }
+
+        public double BalanceCasino
+        {
+            get
+            {
+                return _balanceCasino;
+            }
+            set
+            {
+                //Check correct input!!
+                //kein negativer wert, max. werte, ect...
+            }
+        }
+        #endregion
+
+        #region constructor
+        public Bank (int balancePlayer, int balanceBank)
+        {
+            _balancePlayer = balancePlayer;
+            _balanceCasino = balanceBank;
+        }
+
+        public Bank()
+            //Default Constructor (Welcome Bonus 10€ for Player)
+        {
+            _balanceCasino = 100;
+            _balancePlayer = 10;
+        }
+        #endregion
+
+        #region methods
+        //1. Kredit aufladen/abheben
+        //2. Zwischenspeicher
+        //3. Gewinne/Verluste/Abzug KeSt
+        #endregion
+
     }
 }
