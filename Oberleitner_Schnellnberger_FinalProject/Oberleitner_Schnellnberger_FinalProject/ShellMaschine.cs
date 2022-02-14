970e48c670eb45e93c3f9f2593d5912536ac084b
Christoph Schnellnberger <s2110438023@fhooe.at>
2022-02-14 19:15:07 +0100

    shellgame

--- Oberleitner_Schnellnberger_FinalProject/Oberleitner_Schnellnberger_FinalProject/ShellMaschine.cs
+++ Oberleitner_Schnellnberger_FinalProject/Oberleitner_Schnellnberger_FinalProject/ShellMaschine.cs
@@ -11,6 +11,31 @@ namespace Oberleitner_Schnellnberger_FinalProject
         public static bool ShellGame(Person actualPlayer)
         {
             bool userWin = false;
+            List<int> list = new List<int>();
+            string[] threeCharCard = new string[3];
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            threeCharCard[0] = "\x2666";
+            threeCharCard[1] = "\x2666";
+            threeCharCard[2] = "\x24EA";
+            int[] randomValues=new int[3];
+            Random random = new Random();
+            list.Clear();
+
+            for (int i = 0; i < randomValues.Length; i++)
+            {
+                int value = random.Next(2);
+                if (list.Contains(value))
+                {
+                    continue;
+                    i--;
+                }
+                else
+                {
+                    list.Add(value);
+                }
+            }
+            randomValues = list.ToArray();
+
 
             return userWin;
         }
