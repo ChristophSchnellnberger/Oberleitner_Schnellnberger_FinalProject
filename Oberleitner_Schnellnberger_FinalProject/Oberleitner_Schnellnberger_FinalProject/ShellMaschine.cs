9f2e2efd2b2232da2d421a4fc366b41a24f7b7e3
Christoph Schnellnberger <s2110438023@fhooe.at>
2022-02-14 20:39:38 +0100

    Shellgame debugged

--- Oberleitner_Schnellnberger_FinalProject/Oberleitner_Schnellnberger_FinalProject/ShellMaschine.cs
+++ Oberleitner_Schnellnberger_FinalProject/Oberleitner_Schnellnberger_FinalProject/ShellMaschine.cs
@@ -20,6 +20,8 @@ namespace Oberleitner_Schnellnberger_FinalProject
             threeCharCard[2] = "\x2665";
             int[] randomValues=new int[3];
             Random random = new Random();
+            int choosenValue;
+            int k = 3;
             for (int i = 0; i < 10; i++)
             {
                 list.Clear();
@@ -37,7 +39,7 @@ namespace Oberleitner_Schnellnberger_FinalProject
                     }
                 }
                 randomValues = list.ToArray();
-                int k = 3;
+                k = 3;
                 Console.Clear();
                 foreach (var item in randomValues)
                 {
@@ -50,18 +52,33 @@ namespace Oberleitner_Schnellnberger_FinalProject
                 Console.SetCursorPosition((Console.WindowWidth - 3) / 2, Console.CursorTop);
                 Console.Write("\x2666" + "\x2666" + "\x2666");
             }
-            Console.WriteLine("At which kind of place do you think is the heart? (1,2 or 3)");
+            Console.WriteLine();
+            Console.WriteLine("At which kind of place do you think is the heart? (0, 1 or 2)");
             do
             {
-                int.TryParse(Console.ReadLine(),out int choosenValue);
+                int.TryParse(Console.ReadLine(),out choosenValue);
             }while (false);
 
-           
-
-
-         
-
-
+           if (2 == randomValues[choosenValue])
+            {
+                userWin = true;
+            }
+           else
+            {
+                userWin = false;
+            }
+            Console.WriteLine("The result was: ");
+            k = 3;
+            foreach (var item in randomValues)
+            {
+                Console.SetCursorPosition((Console.WindowWidth - k) / 2, Console.CursorTop);
+                Console.Write(threeCharCard[item]);
+                k = k - 2;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Please press enter for futher actions!");
+            Console.ReadKey();
+            Console.Clear();
             return userWin;
         }
 
